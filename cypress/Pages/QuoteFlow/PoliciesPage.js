0a4883c5bb5e3624fd6b5bf202aa1a84339f1d7f
Mohammed Mohaidat <mohammedmohaidat@Mohammeds-MBP.lan>
2021-11-24 22:29:00 +0300

    log step in policies page

--- cypress/Pages/QuoteFlow/PoliciesPage.js
+++ cypress/Pages/QuoteFlow/PoliciesPage.js
@@ -3,6 +3,7 @@
 export default class PoliciesPage {
 
     verifyPoliciesPageTitle() {
+        cy.log('Check that the user is successfully redirected to the second page of Policies');
         cy.contains("Policies").should('be.visible')
     }
 
